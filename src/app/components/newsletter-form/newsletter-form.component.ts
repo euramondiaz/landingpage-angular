b0aefa835cade15a69fef2061b87c7c3f9e98b8e
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-26 16:12:25 -0300

    first forms

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-newsletter-form',
+  selector: 'newsletter-form',
   standalone: true,
   imports: [
-    BtnPrimaryComponent
+    BtnPrimaryComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
 
+  newsletterForm!: FormGroup;
+
+  constructor() {
+  this.newsletterForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+ }
 }
