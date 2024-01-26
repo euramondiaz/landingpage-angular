c50a3658bf6edadc1614090c5eca694ba81778f2
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-26 12:54:01 -0300

    update home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent, NgOptimizedImage
+    HeaderComponent, NgOptimizedImage, BtnPrimaryComponent
   ],
   providers: [],
   templateUrl: './home.component.html',
