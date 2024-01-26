2165e4e37e802901fb904f5ea2cf090d403a1ee7
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-26 18:00:15 -0300

    update footer and forms

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,15 +3,19 @@ import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NewsletterFormComponent } from '../newsletter-form/newsletter-form.component';
+import { BenefitComponent } from '../benefit/benefit.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     HeaderComponent,
-     NgOptimizedImage,
-     BtnPrimaryComponent,
-     NewsletterFormComponent,
+    NgOptimizedImage,
+    BtnPrimaryComponent,
+    NewsletterFormComponent,
+    BenefitComponent,
+    FooterComponent
   ],
   providers: [],
   templateUrl: './home.component.html',
